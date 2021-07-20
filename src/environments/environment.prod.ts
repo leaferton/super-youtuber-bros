0aabc32384f6582fada97bd203676b69a63fb1bc
John Eisenhardt <johneisenhardt@gmail.com>
2021-07-20 12:56:54 -0400

    added firebase web hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyA9_eh5Cvncs6zCC-hp43ewGrwjsHI96eA",
+	  authDomain: "super-bros-fd6b4.firebaseapp.com",
+	  projectId: "super-bros-fd6b4",
+	  storageBucket: "super-bros-fd6b4.appspot.com",
+	  messagingSenderId: "976682102637",
+	  appId: "1:976682102637:web:8436b12075dcf02b927464",
+	  measurementId: "G-1QF9388T9P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyA9_eh5Cvncs6zCC-hp43ewGrwjsHI96eA",
+	  authDomain: "super-bros-fd6b4.firebaseapp.com",
+	  projectId: "super-bros-fd6b4",
+	  storageBucket: "super-bros-fd6b4.appspot.com",
+	  messagingSenderId: "976682102637",
+	  appId: "1:976682102637:web:8436b12075dcf02b927464",
+	  measurementId: "G-1QF9388T9P"
+  }
 };
 
 /*
