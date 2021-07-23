facc5ef07c973d910c1116861093a9d19f635a1a
John Eisenhardt <johneisenhardt@gmail.com>
2021-07-23 12:25:26 -0400

    made search bar, you can now add and remove youtube channels, and  you are now able to short cut to the youtubers by clicking on their names on dashboard (or by  searching it).

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+		  { id: 1, rick:false, name: 'Pewdiepie', description: 'Whats Going on bros my names PEEWWDIEEEEPIEEEE', url: 'https://www.youtube.com/user/pewdiepie', video:'https://www.youtube.com/watch?v=6Dh-RL__uN4&ab_channel=PewDiePie'},
+		  { id: 2, rick:false, name: 'Mr.Beast', description: 'Will drown you with his vast amounts of money', url: 'https://www.youtube.com/user/MrBeast6000', video:'https://www.youtube.com/watch?v=NkE0AMGzpJY&ab_channel=MrBeast'},
+		  { id: 3, rick:false, name: 'Technoblade', description: 'The second worst thing to happen to those orphans', url: 'https://www.youtube.com/user/technothepig', video:'https://www.youtube.com/watch?v=5qjnDd1rsII&ab_channel=Technoblade'},
+		  { id: 4, rick:false, name: 'Dream', description: 'He will cancel you on twitter with his stan army', url: 'https://www.youtube.com/user/DreamTraps', video:'https://www.youtube.com/watch?v=tylNqtyj0gs&t=1250s&ab_channel=Dream'},
+		  { id: 5, rick:false, name: 'Dani', description: 'He likes milk', url: 'https://www.youtube.com/channel/UCIabPXjvT5BVTxRDPCBBOOQ', video:'https://youtu.be/K0X5A6aURms'},
+		  { id: 6, rick:false, name: 'Tommyinnit', description: '"Just killed a woman feeling good"', url: 'https://www.youtube.com/channel/UC5p_l5ZeB_wGjO_yDXwiqvw', video:'https://youtu.be/RWH10upb4h4'},
+		  { id: 7, rick:false, name: 'Slimecicle', description: 'Dap Me Up', url: 'https://www.youtube.com/c/Slimecicle/videos?view=0&sort=p&flow=grid', video:'https://www.youtube.com/watch?v=j8qp3ITVqY0&ab_channel=Slimecicle'},
+		  { id: 8, rick:false, name: 'Jshlatt', description: '"Truly disgusting that some “people” would do this horrible thing."', url: 'https://www.youtube.com/channel/UC2mP7il3YV7TxM_3m6U0bwA', video:'https://www.youtube.com/watch?v=Iy-Z6S7cFJc&ab_channel=jschlattLIVE'},
+		  { id: 9, rick:false, name: 'Leaferton', description: 'WASSUP (best youtube channel ever, this is totally unbiased)', url: 'https://www.youtube.com/channel/UCHV-7RiAJqczPdV5MwA2-cQ', video: 'https://www.youtube.com/watch?v=4CVII0RXfWc&t=51s&ab_channel=Leaferton'},
+		  { id: 10, rick:true, name: 'Secret Youtuber', description: '', url: '', video:''},
+		];
+    return {heroes};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1: 11;
+  }
+}
